fe1f7f21f39444ed6b3c37537df459f4afb9fc24
Richard Trapanese <richardtrapanese@gmail.com>
2018-05-10 20:18:44 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,8 +3,15 @@
 def countdown(number)
   counter = 0
   while counter < number
+    countdown_with_sleep()
     puts "#{number} SECOND(S)!"
     number -= 1
   end
   return "HAPPY NEW YEAR!"
+end
+def countdown_with_sleep(number)
+  counter = 0
+  while counter < number
+    counter += 1
+  end
 end
